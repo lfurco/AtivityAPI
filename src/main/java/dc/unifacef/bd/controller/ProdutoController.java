836a553194b48dbb00c44b8814882b1888a6af16
lfurco <luquinharighetto98@gmail.com>
2026-05-28 19:58:52 -0300

    Att API

--- src/main/java/dc/unifacef/bd/controller/ProdutoController.java
+++ src/main/java/dc/unifacef/bd/controller/ProdutoController.java
@@ -4,10 +4,12 @@ import dc.unifacef.bd.model.Produto;
 import dc.unifacef.bd.service.ProdutoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController // Ele recebe as requisições HTTP
 @RequestMapping("/produtos")
@@ -25,5 +27,14 @@ public class ProdutoController {
     @GetMapping
     public ResponseEntity<List<Produto>> listar(){
         return ResponseEntity.ok(service.listar()); // statusCode: 200 - OK
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Produto>> buscarPorId(@PathVariable Long id){
+        Optional<Produto> prod = service.buscarPorId(id);
+        if(prod.isEmpty()){
+            return ResponseEntity.notFound().build();    // statusCode: 404 - Produto não existente
+        }
+        return ResponseEntity.ok(prod);     // statussCode: 200 - Retorna produto encontrado
     }
 }
